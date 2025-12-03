2f76536853a7746ef7b5026070eff861fd6861df
gohel-darshan <darshanrgohel@gmail.com>
2025-12-03 11:37:10 +0530

    cors setup

--- server.js
+++ server.js
@@ -23,7 +23,16 @@ const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://home-service-forntend.vercel.app'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
